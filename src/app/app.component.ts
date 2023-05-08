14ff77f756e3ac7a989ecaa74b7585f12c5ac82b
Esteban Salazar Boamorte <salazar.z.esteban@gmail.com>
2023-05-08 15:47:11 -0500

    feat: :sparkles: test expand operator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { delay, expand, fromEvent, map, of, take } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,14 @@ import { RouterModule, RouterOutlet } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor() {
+    const clicks = fromEvent(document, 'click');
+    const powersOfTwo = clicks.pipe(
+      map(() => 1),
+      expand(x => of(2 * x).pipe(delay(1000))),
+      take(10)
+    );
+    powersOfTwo.subscribe(x => console.log(x));
+  }
 }
